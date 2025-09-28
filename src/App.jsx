78d777f912db544ac0897777ba781f0d0c9a0916
aartiPurohit <rti.raj.15nov2002@gmail.com>
2025-09-28 15:44:36 +0530

    complete-mouse-pointer-using-useEffect()

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./app.css"
 
 const App = () => {
   const [mousePosition, setMousePosition] = useState([0, 0]);
 
-  const handleMouseMove = (e) => {
+  useEffect(() => {
+    const handleMouseMove = (e) => {
     setMousePosition(
       [mousePosition[0] = e.movementX, mousePosition[1] = e.movementY]
     )
     console.log(mousePosition)
   }
+  window.addEventListener('mousemove', (e) => {handleMouseMove(e)})
+  });
+
+  
   return (
-    <div onMouseMove={handleMouseMove}>
+    <div>
       <h1>Mouse Pointer</h1>
       <p>X: {mousePosition[0]}</p>
       <p>Y: {mousePosition[1]}</p>
